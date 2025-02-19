85fd6c2850d90bcab6d64ac4e26dc84f78598bbe
MaryMachuma <machhmary173@gmail.com>
2025-02-19 16:38:43 +0300

    Completed Python init and self assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+# Test the class by creating instances (should be outside the class)
+if __name__ == "__main__":
+    fido = Dog("Fido")
+    snoopy = Dog("Snoopy", "Beagle")
+
+    print(f"{fido.name} is a {fido.breed}.")  # Expected: Fido is a Mutt.
+    print(f"{snoopy.name} is a {snoopy.breed}.")  # Expected: Snoopy is a Beagle.
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+# Test the class by creating an instance
+if __name__ == "__main__":
+    sophie = Person("Sophie")
+    print(f"Person's name is {sophie.name}.") # Expected: Person's nsme is Sophie
